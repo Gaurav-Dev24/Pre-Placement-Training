6585640938ce3823713cbe397ceec64d527e735b
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-07-12 22:30:38 +0530

    Core Module Assignment 7 question 2 solved

--- Core Module Assignment/Assignment 7/Q2/script.js
+++ Core Module Assignment/Assignment 7/Q2/script.js
@@ -5,14 +5,7 @@ const seconds = document.getElementById("seconds");
 
 function updateTime() {
   const now = new Date();
-  hours.innerHTML = now.getHours();
-  let ampm = 'AM';
-  
-  if (hours > 12) {
-    hours -= 12;
-    ampm = 'PM';
-  }
-
+  hours.innerHTML = String(now.getHours()).padStart(2, '0');
   minutes.innerHTML = String(now.getMinutes()).padStart(2, '0');
   seconds.innerHTML = String(now.getSeconds()).padStart(2, '0');
 }
